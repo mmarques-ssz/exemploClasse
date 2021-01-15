04eaf6c14f6fefd961a72ca24003eac7b7ffcfac
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2021-01-15 17:09:10 -0300

    Add files via upload

---
+++ testePOO/testePOO/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace testePOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TesteClasse tc;
+            tc = new TesteClasse();
+
+            Console.Write("Informe o primeiro valor: ");
+            tc.x = int.Parse(Console.ReadLine());
+
+            Console.Write("Informe o segundo valor: ");
+            tc.y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Soma: {0}", tc.somar());
+            Console.WriteLine("Subtração: {0}", tc.subtrair());
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ testePOO/testePOO/TesteClasse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace testePOO
+{
+    class TesteClasse
+    {
+        public int x;
+
+        public int y;
+
+        public int somar()
+        {
+            return x + y;
+        }
+
+        public int subtrair()
+        {
+            return x - y;
+        }
+    }
+}
